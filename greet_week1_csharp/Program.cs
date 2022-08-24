2f73a3d76fcf4d268214e8d6da5768205018a8c9
yeffrey espinoza <yeffrey4008@gmail.com>
2022-08-24 17:52:11 -0500

    create my directory and create my first project for university

---
+++ greet_week1_csharp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace greet_week1_csharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //variables
+            string name;
+            //entrada
+            Console.Write("type your name: ");
+            name = Console.ReadLine();
+
+            //proceso ---
+
+            //salida
+            Console.WriteLine("Hello " + name);
+            Console.ReadKey();
+        }
+    }
+}
